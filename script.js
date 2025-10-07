d4ac85f1ab81124b0a7bd0d27c78f86d169bebab
Rich <richbport@yahoo.com>
2025-10-07 18:05:13 -0400

    async try

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function makeCoffee(beans) {
-    try {
-        if (!beans) throw new Error("BEANS EMPTY!"); // Yelling
-        return "🍵";
-    } catch (error){
-      console.error("Coffee emergency:", error); // Log the yell
-      return "Couldn't make coffee"; // Graceful fallback
-    }
+// Basic Try-Catch (Synchronous)
+
+// function makeCoffee(beans) {
+//     try {
+//         if (!beans) throw new Error("BEANS EMPTY!"); // Yelling
+//         return "🍵";
+//     } catch (error){
+//       console.error("Coffee emergency:", error); // Log the yell
+//       return "Couldn't make coffee"; // Graceful fallback
+//     }
+// }
+
+//  Async Error Handling
+
+async function orderPizza() {
+  try {
+    const menu = await fetchMenu();
+    const pizza = await cookPizza(menu);
+    return deliver(pizza);
+  } 
 }
